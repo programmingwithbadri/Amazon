1c9534a44532248adc27a06ec1ad73afe33bf7a8
Badri Narayanan <programmingwithbadri@outlook.com>
2021-11-04 21:05:03 +0530

    create the home screen and add product component to Home

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCBduPmDEtD5C-GsNavM69LRqiKXpH_KPM",
+    authDomain: "fir-dbde5.firebaseapp.com",
+    projectId: "fir-dbde5",
+    storageBucket: "fir-dbde5.appspot.com",
+    messagingSenderId: "182973786520",
+    appId: "1:182973786520:web:84d2fdb3f44e8adf82a3ed",
+    measurementId: "G-KRFCN10PLQ"
+};
